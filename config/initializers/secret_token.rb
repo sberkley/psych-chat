cff9f7843e90d30cb2cdc819577428ac906e2f7c
Scott Berkley <berkley47@gmail.com>
2012-06-02 17:42:35 -0400

    default rails starting files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PsychChat::Application.config.secret_token = '8a382903f8cfeaf95b0e155717219dab73b587c03660f889cd290f6b23e30efe5cd29cf20e22385eb411223f8290cf70204bbbbe37e1787b8f0fb8f19d46ade2'
